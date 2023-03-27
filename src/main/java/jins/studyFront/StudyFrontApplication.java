dfd88ffdc7500cbe81ad491427c7c61f1d030d30
YangJinSoo94 <jschrome782@gmail.com>
2023-03-27 16:52:19 +0900

    first

---
+++ src/main/java/jins/studyFront/StudyFrontApplication.java
@@ -0,0 +1,13 @@
+package jins.studyFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jins/studyFront/StudyFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package jins.studyFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
